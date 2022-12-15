494415738397d9ac554e0dc9c0ac37176e189e5d
Fahad <mfahad1667@gmail.com>
2022-12-15 18:34:04 +0500

    Update app.js file

--- app.js
+++ app.js
@@ -6,13 +6,18 @@
 //name('Fahad' , 34);
 
 
+
 // ********** Module **********
 // console.log(module);
+
+
 
 // ********** Loading a Module **********
 // const logger = require("./logger");
 
 // logger("Fahad");
+
+
 
 // ********** Path Module **********
 
@@ -24,6 +29,9 @@
 // console.log(obj);
 // console.log(obj2);
 
+
+
+
 // ********** OS Module **********
 
 // const os = require('os');
@@ -33,6 +41,7 @@
 
 // console.log('Total Memory: ' + totalMemory);
 // console.log(`Free Memory: ${freeMemory}`);
+
 
 
 // // ********** FileSystem Module **********
@@ -50,3 +59,60 @@
 // else
 // console.log('Result:' , files);
 // });
+
+
+
+// // ********** Event Module **********
+
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+
+// // Resister an Listener
+// emitter.on('messageLogged', function(){
+//     console.log('Listener');
+// });
+
+// // Raise an Event
+// emitter.emit('messageLogged');
+
+// // Resister an Listener that takes arguments
+// emitter.on('messageLogged', (arg) => {
+//     console.log('Listener', arg);
+// });
+
+// // Raise an Event that pass arguments
+// emitter.emit('messageLogged', { id: 1, url: 'https//log.io'} );
+
+
+//******************* HTTP Module ***************
+
+// Modern Way to Create Server
+
+// const http = require('http');
+// const server = http.createServer((req , res) => {
+// if (req.url === '/')
+// {
+//     res.write('Welcome to Node JS');
+//     res.end();
+// }
+// if (req.url === '/api/courses')
+// {
+//     res.write(JSON.stringify([1,2,3]));
+//     res.end();
+// }
+
+// });
+
+// common Method to Create Sever
+// server.on('connection', (socket) => {
+//     console.log('New connection');
+// });
+
+// server.listen(3000);
+// console.log('Listening on Port 3000');
+
+
+
+// That server cnnects both Methods.
+// server.listen(3000);
+// console.log('Listening on Port 3000');
